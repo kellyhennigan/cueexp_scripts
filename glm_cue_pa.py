1142d797ac296d9491e41416e1b0573b47394527
kellyhennigan <kelhennigan@gmail.com>
2017-11-03 21:53:18 -0700

    edits

--- glm_cue_pa.py
+++ glm_cue_pa.py
@@ -20,7 +20,7 @@ afniStr = '_afni' # set this to '' if not using afni coreg version
 func_dir = 'func_proc'  	# relative to subject-specific directory
 func_files = 'pp_cue_tlrc'+afniStr+'.nii.gz'
 
-out_dir = os.path.join(data_dir,'results_cue'+afniStr+'_na')  	# directory for out files 
+out_dir = os.path.join(data_dir,'results_cue'+afniStr+'_pa_cond')  	# directory for out files 
 out_str = 'glm'					# string for output files
 
 
@@ -78,12 +78,16 @@ for subject in subjects:
 		#'-stim_file 10 regs/img_cuec.1D -stim_label 10 img '
 		'-stim_file 11 regs/choice_cuec.1D -stim_label 11 choice ' 
 		'-stim_file 12 regs/choicert_cuec.1D -stim_label 12 choice_rt ' 
-		'-stim_file 13 regs/trial_cuec.1D -stim_label 13 trial ' 
-		'-stim_file 14 regs/na_trial_cuec.1D -stim_label 14 na ' 
-		# '-stim_file 13 regs/alcohol_trial_cuec.1D -stim_label 13 alcohol ' 
-		# '-stim_file 14 regs/drugs_trial_cuec.1D -stim_label 14 drugs ' 
-		# '-stim_file 15 regs/food_trial_cuec.1D -stim_label 15 food ' 
-		# '-stim_file 16 regs/neutral_trial_cuec.1D -stim_label 16 neutral ' 
+		# '-stim_file 13 regs/trial_cuec.1D -stim_label 13 trial ' 
+		# '-stim_file 14 regs/na_trial_cuec.1D -stim_label 14 na ' 
+		'-stim_file 13 regs/alcohol_trial_cuec.1D -stim_label 13 alcohol ' 
+		'-stim_file 14 regs/paalcohol_trial_cuec.1D -stim_label 14 pa_alcohol ' 
+		'-stim_file 15 regs/drugs_trial_cuec.1D -stim_label 15 drugs ' 
+		'-stim_file 16 regs/padrugs_trial_cuec.1D -stim_label 16 pa_drugs ' 
+		'-stim_file 17 regs/food_trial_cuec.1D -stim_label 17 food ' 
+		'-stim_file 18 regs/pafood_trial_cuec.1D -stim_label 18 pa_food ' 
+		'-stim_file 19 regs/neutral_trial_cuec.1D -stim_label 19 neutral ' 
+		'-stim_file 20 regs/paneutral_trial_cuec.1D -stim_label 20 pa_neutral ' 
 		# '-num_glt 5 '					 # of contrasts
 		# '-glt_label 1 alcohol-neutral -gltsym "SYM: +alcohol -neutral" ' 
 		# '-glt_label 2 drugs-neutral -gltsym "SYM: +drugs -neutral" '
