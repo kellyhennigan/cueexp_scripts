a24913c32bc55d7214ae875977648e09778c2c0c
kellyhennigan <kelhennigan@gmail.com>
2017-11-05 20:23:59 -0800

    edits

--- calculateBrainBehCorr_roi_script.m
+++ calculateBrainBehCorr_roi_script.m
@@ -34,7 +34,7 @@ subjects(idx)=[]; gi(idx)=[];
 
 
 %%%%%%%%%%%%%% brain data
-seedRoiName = 'VTA';
+seedRoiName = 'ins_desai';
 
 % name of dir to save to where %s is: subject, roi, stim
 inFile = fullfile(dataDir,'%s',['single_trial_' task '_timecourses'],'%s','%s');
