373a0b1ad931606749a1729e5c471e302022ae89
kellyhennigan <kelhennigan@gmail.com>
2019-02-05 18:55:22 -0800

    edits

---
+++ dmri/getDTIColors_test.m
@@ -0,0 +1,62 @@
+function colors = getDTIColors_test(targets)
+% -------------------------------------------------------------------------
+% usage: returns rgb color values for plotting cue experiment results. The
+% idea of having this is to keep plot colors for each stimulus consistent.
+% Hard code desired colors here, then they will be used by various plotting
+% scripts.
+
+
+% INPUT:
+
+% OUTPUT:
+%   colors - rgb values in rows for colors
+%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+if ~iscell(targets)
+    targets = {targets};
+end
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%% define colors for all possible stims/groups here %%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% yellow=[10 30 100]./255;
+yellow=[230 171 2]./255;
+
+green=[102 166 30]./255;
+pink=[231 41 138]./255;
+purple=[117 112 179]./255;
+orange=[217 95 2]./255;
+junglegreen=[27 158 119]./255;
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%% determine which colors to return based on input labels %%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+colors = [];
+for i=1:numel(targets)
+      
+    switch lower(targets{i})
+        
+        case 'nacc'
+            colors(i,:) = yellow;
+            
+        case 'caudate'
+            colors(i,:) = pink;
+            
+        case 'putamen'
+            colors(i,:) = purple;
+            
+        otherwise
+            colors(i,:)=green;
+            
+    end
+    
+end
+            
+       
