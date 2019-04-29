a6ac1626e90f2a8cf3a2ceed08c7331b373036f9
kellyhennigan <kelhennigan@gmail.com>
2019-04-29 15:25:08 -0700

    edits

--- dmri/plotFgNodePointsOnT1_script.m
+++ dmri/plotFgNodePointsOnT1_script.m
@@ -67,8 +67,8 @@ for lr=LorR
                 sprintf(xform_aff,subject),...
                 sprintf(xform_invWarp,subject)));
             
-            % get img coords for a sphere around mean coord w/2 voxel radius
-            a=dtiBuildSphereCoords(mrAnatXformCoords(ol.qto_ijk,node_coords_tlrc(i,:)),2);
+            % get img coords for a sphere around mean coord w/4 voxel radius
+            a=dtiBuildSphereCoords(mrAnatXformCoords(ol.qto_ijk,node_coords_tlrc(i,:)),4);
             
             % get index for those sphere coords
             idx=sub2ind(size(t1.data),a(:,1),a(:,2),a(:,3));
