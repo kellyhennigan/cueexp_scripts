536ac62b4c4185e3e799cef3f9a59b449d4763eb
kellyhennigan <kelhennigan@gmail.com>
2019-07-15 14:04:36 -0700

    edits

---
+++ dmri/getCueBaseDir.m
@@ -0,0 +1,20 @@
+function baseDir = getCueBaseDir()
+% -------------------------------------------------------------------------
+% usage: function to get path to cue experiment base directory, which is
+% different depending on which computer this function is running on
+% 
+% OUTPUT:
+%   baseDir - string specifiying data directory path
+% 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+cName=getComputerName;
+
+if strcmp(cName,'cnic2')               % cni server
+    baseDir = '/home/hennigan/cueexp';
+elseif strcmp(cName,'vta')               % vta server
+    baseDir = '/home/span/lvta/cueexp';
+else                                   % assume its my laptop
+    baseDir = '/Users/kelly/cueexp';
+end
