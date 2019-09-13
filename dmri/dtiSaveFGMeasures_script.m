354a5475cece42f8060f1971727e56895e308f9f
kellyhennigan <kelhennigan@gmail.com>
2019-09-13 14:17:39 -0700

    edits

--- dmri/dtiSaveFGMeasures_script.m
+++ dmri/dtiSaveFGMeasures_script.m
@@ -25,14 +25,14 @@ LorR = ['L','R'];
 
 combineLR = 1; % 1 to combine L and R, otherwise, 0
 
-% seeds = {'DA','DA','DA','DA'};
-% targets = {'nacc','nacc','caudate','putamen'};
-% versionStrs = {'aboveAC_v2_autoclean','v2_autoclean','v2_autoclean','v2_autoclean'};
+seeds = {'DA','DA'};
+targets = {'caudate','putamen'};
+versionStrs = {'autoclean','autoclean'};
 
 
-seeds = {'DA'};
-targets = {'nacc'};
-versionStrs = {'aboveAC_autoclean'};
+% seeds = {'DA'};
+% targets = {'nacc'};
+% versionStrs = {'aboveAC_autoclean'};
 
 
 % 
