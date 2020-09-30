deba53e77071b2b92a55fdf05a2227fc226808c5
kellyhennigan <kelhennigan@gmail.com>
2020-09-30 11:47:17 -0700

    edits

--- getMIDFC_script.m
+++ getMIDFC_script.m
@@ -23,22 +23,31 @@ task='mid';
 
 Xbasefname=['pp_' task '_tlrc_afni_nuisance_designmat.txt'];
 
-seedtsfname=[task '_VTA_afni.1D']; % seed ROI time series
+seed = 'mpfc';
+seedtsfname=[task '_' seed '_afni.1D']; % seed ROI time series
 
-targettsfname=[task '_nacc_afni.1D']; % time series for target ROI
+target = 'nacc';
+targettsfname=[task '_' target '_afni.1D']; % time series for target ROI
 
-conds = {'gain5','gain0'}; % conditions to contrast
+% conds = {'gain5','gain0'}; % conditions to contrast
+conds = {'gainwin','gainmiss'}; % conditions to contrast
+
 
 regfileStr=fullfile(dataDir,'%s','regs',['%s_trial_mid.1D']); %s is subject, conds
 
-censorTRs=1; % 1 to censor out bad motion TRs, otherwise 0
+censorTRs=0; % 1 to censor out bad motion TRs, otherwise 0
 
 censorFilePath = fullfile(dataDir, '%s','func_proc',[task '_censor.1D']);
 
-TR=2; 
+TR=2;
 
 fcTRs=[1:7]; % which TRs to use for func connectivity calculation?
-% This should be relative to trial onset, e.g., TRs=[3 4] 
+% This should be relative to trial onset, e.g., TRs=[3 4]
+
+% filepath for saving out table of variables
+outDir=fullfile(dataDir,'funcconn_measures');
+outPath = fullfile(outDir,[task '_' seed '_' target '_funcconn.csv']);
+
 
 %% do it
 
@@ -51,118 +60,115 @@ for s=1:numel(subjects)
     cd(subject); cd func_proc;
     
     % load baseline model that includes all baseline regs
+    Xbase=readtable(sprintf(Xbasefname,subject));
+    baseNames = Xbase.Properties.VariableNames;
+    Xbase=table2array(Xbase);
     
-    if exist(Xbasefname,'file')
-        
-        Xbase=readtable(sprintf(Xbasefname,subject));
-        baseNames = Xbase.Properties.VariableNames;
-        Xbase=table2array(Xbase);
-      
-        seedts=dlmread(seedtsfname);
-        targetts=dlmread(targettsfname);
-     
-          % regress out baseline regs
-          seed_errts=glm_fmri_fit(seedts,Xbase,[],'err_ts');
-        target_errts=glm_fmri_fit(targetts,Xbase,[],'err_ts');
-       
-        % if censoring TRs, set censored TRs to nan
-        if censorTRs
-            censorVols = find(dlmread(sprintf(censorFilePath,subject))==0);
-            seedts(censorVols)=nan; seed_errts(censorVols)=nan;
-            targetts(censorVols)=nan; target_errts(censorVols)=nan;
-        end
-          
-        % method 1: straight up correlation 
-        r_restingstate(s,1)=nancorr(seedts,targetts);
-        
-        % method 2: correlation, partialling out nuisance regs 
-%         note: this returns the same r as doing
-%         partialcorr(targets,seedts,Xbase), but nancorr() can handle nan
-%         values
-        r_restingstate_partial(s,1)=nancorr(seed_errts,target_errts);
-        
-        
-        %% contrast functional connectivity between gain5 vs gain0 trials
-        for j=1:numel(conds)
-            
-            onsetTRs=find(dlmread(sprintf(regfileStr,subject,conds{j})));
-            
-              TRs=repmat(onsetTRs,1,fcTRs(end))+repmat(0:fcTRs(end)-1,size(onsetTRs,1),1);
-              TRs=TRs(:,fcTRs);
-              
-              if ~censorVols
-         
-                  % correlation between seed and target during cond{j}
-                  r_cond{j}(s,:)=diag(corr(seedts(TRs),targetts(TRs)));
-                  
-                  % also do it on data with nuisance regs regressed out
-                  r_cond_partial{j}(s,:)=diag(corr(seed_errts(TRs),target_errts(TRs)));
-                  
-              else
-                  
-                  % if censorVols, have to do this the slower way with a loop:
-                  for k=1:numel(fcTRs)
-                      r_cond{j}(s,k)=nancorr(seedts(TRs(:,k)),targetts(TRs(:,k)));
-                      r_cond_partial{j}(s,k)=nancorr(seed_errts(TRs(:,k)),target_errts(TRs(:,k)));
-                  end
-          
-                  
-       r_cond{1} = 
-            
-            
-            
-            
-            
-        % do gain5 vs gain0 anticipation
-        onsetTRs1=find(dlmread(sprintf(reg1file,subject)));
-        onsetTRs2=find(dlmread(sprintf(reg2file,subject)));
-        
-          % get array of indices of which TRs to get data for
-          TRs1=repmat(onsetTRs1,1,fcTRs(end))+repmat(0:fcTRs(end)-1,size(onsetTRs1,1),1);
-          TRs1=TRs1(:,fcTRs);
-          TRs1=reshape(TRs1',[],1);
-          
-          TRs2=repmat(onsetTRs2,1,fcTRs(end))+repmat(0:fcTRs(end)-1,size(onsetTRs2,1),1);
-          TRs2=TRs2(:,fcTRs); 
-          TRs2=reshape(TRs2',[],1);
-          
-          if censorTRs
-              censorVols = find(dlmread(sprintf(censorFilePath,subject))==0);
-              TRs1(ismember(TRs1,censorVols))=nan;
-              TRs2(ismember(TRs2,censorVols))=nan;
-          end
-            
-          TRs1=TRs1(~isnan(TRs1));
-          TRs2=TRs2(~isnan(TRs2));
-          
-          % method 1: use error time series
-          r1(s,1)=corr(seed_errts(TRs1),target_errts(TRs1));
-          r2(s,1)=corr(seed_errts(TRs2),target_errts(TRs2));
-         
-          % method 2: use regular time series
-          r21(s,1)=corr(seedts(TRs1),targetts(TRs1));
-          r22(s,1)=corr(seedts(TRs2),targetts(TRs2));
-         
-         
-    else
-        
-        d1(s,1)=nan;
-        d2(s,1)=nan;
-        d3(s,1)=nan;
-        
-        r1(s,1)=nan;
-        r2(s,1)=nan;
-        
-        r21(s,1)=nan;
-        r22(s,1)=nan;
-        
+    seedts=dlmread(seedtsfname);
+    targetts=dlmread(targettsfname);
+    
+    % regress out baseline regs
+    seed_errts=glm_fmri_fit(seedts,Xbase,[],'err_ts');
+    target_errts=glm_fmri_fit(targetts,Xbase,[],'err_ts');
+    
+    % if censoring TRs, set censored TRs to nan
+    if censorTRs
+        censorVols = find(dlmread(sprintf(censorFilePath,subject))==0);
+        seedts(censorVols)=nan; seed_errts(censorVols)=nan;
+        targetts(censorVols)=nan; target_errts(censorVols)=nan;
     end
     
-    cd(dataDir)
+    % method 1: straight up correlation
+    r_restingstate(s,1)=nancorr(seedts,targetts);
+    
+    % method 2: correlation, partialling out nuisance regs
+    %         note: this returns the same r as doing
+    %         partialcorr(targets,seedts,Xbase), but nancorr() can handle nan
+    %         values
+    r_restingstate_partial(s,1)=nancorr(seed_errts,target_errts);
+    
+    
+    %% contrast functional connectivity between gain5 vs gain0 trials
+    
+    %     varnames = {}; % variable names
+    %     fcvals = []; % matrix that will hold FC correlation values
+    
+    for j=1:numel(conds)
+        
+        onsetTRs=find(dlmread(sprintf(regfileStr,subject,conds{j})));
+        
+        % keep track of the number of trials per cond for each subj
+        ntrials(s,j)=numel(onsetTRs);
+        
+        TRs=repmat(onsetTRs,1,fcTRs(end))+repmat(0:fcTRs(end)-1,size(onsetTRs,1),1);
+        TRs=TRs(:,fcTRs);
+        
+        if ~censorTRs
+            
+            % correlation between seed and target during cond{j}
+            r_cond{j}(s,:)=diag(corr(seedts(TRs),targetts(TRs)));
+            
+            % also do it on data with nuisance regs regressed out
+            r_cond_partial{j}(s,:)=diag(corr(seed_errts(TRs),target_errts(TRs)));
+            
+        else
+            
+            % if censorVols, have to do this the slower way with a loop:
+            for k=1:numel(fcTRs)
+                r_cond{j}(s,k)=nancorr(seedts(TRs(:,k)),targetts(TRs(:,k)));
+                r_cond_partial{j}(s,k)=nancorr(seed_errts(TRs(:,k)),target_errts(TRs(:,k)));
+            end
+            
+        end
+        
+        %          % update var names
+        %         for ti = 1:numel(fcTRs)
+        %             varnames{end+1} = ['FC_' conds{j} '_TR' num2str(TRs(ti))];
+        %         end
+        
+    end % cond loop
+    
+    cd(dataDir);
     
 end % subject loop
 
+%% save everything out into a table
 
-fc1=r1-r2;
-fc2=r21-r22;
+%%%%% NOTE THIS PART IS HARDCODED FOR 2 CONDITIONS!!
+%%%%% this should be fixed at some point!!!!
+
+varnames = {};
+for j=1:numel(conds)
+    for ti = 1:numel(fcTRs)
+        varnames{end+1} = ['FC_' conds{j} '_TR' num2str(fcTRs(ti))];
+    end
+end
+
+for j=1:numel(conds)
+    for ti = 1:numel(fcTRs)
+        varnames{end+1} = ['FCpartial_' conds{j} '_TR' num2str(fcTRs(ti))];
+    end
+end
+
+Ttask = array2table([r_cond{1} r_cond{2} r_cond_partial{1} r_cond_partial{2}],'VariableNames',varnames);
+
+Trestingstate= array2table([r_restingstate r_restingstate_partial],'VariableNames',{'FCcorr_restingstate','FCpartialcorr_restingstate'});
+
+subjid = cell2mat(subjects);
+Tsubj = table(subjid);
+Tgroupindex=table(gi);
+
+% concatenate all data into 1 table
+T=table();
+% T = [Tsubj Tgroupindex Tvars Tbrain Tdti Tnvox];
+
+T = [Tsubj Tgroupindex Ttask Trestingstate];
+% T = [Tsubj Tgroupindex Tvars Tdti Tcontrollingagemotion];
+
+% save out
+writetable(T,outPath);
+
+
+
+
 
